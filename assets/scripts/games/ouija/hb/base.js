594f77fc59f284e44f5f9830da00f511eb2c769c
Seraf Dos Santos <seraf@cyb3r.ca>
2021-04-14 03:35:50 -0400

    game boards basic templates and contents

---
+++ assets/scripts/games/ouija/hb/base.js
@@ -0,0 +1,17 @@
+/*
+ *	Author: Serafim Junior Dos Santos Fagundes
+*/
+iImgs = 27;         // number of cards in deck
+sCB = '000.jpg';   // cardback id
+
+function populateArray(aCrds) {
+	for (var j=1;j<=27;j++) {
+		aCrds.push("../../../assets/images/cards/hb/w300px/ho-"+j+".jpg");
+	}
+	return aCrds;
+}
+
+$(function () {
+    aRndCrds = populateArray(aRndCrds)
+    aRndCrds = shuffle(aRndCrds);
+});
